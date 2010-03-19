83f59d18f6da5c24ae54624915b83330a4945ddd
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-03-19 11:00:29 +0100

    Remove explicit module declaration.
    Having modules declared in an explicit manner somehow messes
    with ocamlyacc, preventing it to work.

--- pity/name.ml
+++ pity/name.ml
@@ -1,22 +1,18 @@
-module Name = struct
+type name =
+      Name of string
 
-    type name =
-          Name of string
+let to_string = function
+      Name(x) -> x
 
-    let to_string = function
-          Name(x) -> x
-
-    let rec list_to_string = function
-          [] -> (
-              ""
-          )
-        | [x] -> (
-              (to_string x)
-          )
-        | x::y -> (
-              (to_string x) ^
-              "," ^
-              (list_to_string y)
-          )
-
-end
+let rec list_to_string = function
+      [] -> (
+          ""
+      )
+    | [x] -> (
+          (to_string x)
+      )
+    | x::y -> (
+          (to_string x) ^
+          "," ^
+          (list_to_string y)
+      )
--- pity/term.ml
+++ pity/term.ml
@@ -1,48 +1,44 @@
 open Name
 
-module Term = struct
+type term =
+      Nil
+    | Input of Name.name * Name.name list
+    | Output of Name.name * Name.name list
+    | Restriction of Name.name * term
+    | Composition of term * term
+    | Prefix of term * term
 
-    type term =
-          Nil
-        | Input of Name.name * Name.name list
-        | Output of Name.name * Name.name list
-        | Restriction of Name.name * term
-        | Composition of term * term
-        | Prefix of term * term
-
-    let rec to_string = function
-          Nil -> (
-              "0"
-          )
-        | Input(x, y) -> (
-              (Name.to_string x) ^
-              "(" ^
-              (Name.list_to_string y) ^
-              ")"
-          )
-        | Output(x, y) -> (
-              "!" ^
-              (Name.to_string x) ^
-              "<" ^
-              (Name.list_to_string y) ^
-              ">"
-          )
-        | Restriction(x, p) -> (
-              "(v" ^
-              (Name.to_string x) ^
-              ")(" ^
-              (to_string p) ^
-              ")"
-          )
-        | Composition(p, q) -> (
-              (to_string p) ^
-              "|" ^
-              (to_string q)
-          )
-        | Prefix(p, q) -> (
-              (to_string p) ^
-              "." ^
-              (to_string q)
-          )
-
-end
+let rec to_string = function
+      Nil -> (
+          "0"
+      )
+    | Input(x, y) -> (
+          (Name.to_string x) ^
+          "(" ^
+          (Name.list_to_string y) ^
+          ")"
+      )
+    | Output(x, y) -> (
+          "!" ^
+          (Name.to_string x) ^
+          "<" ^
+          (Name.list_to_string y) ^
+          ">"
+      )
+    | Restriction(x, p) -> (
+          "(v" ^
+          (Name.to_string x) ^
+          ")(" ^
+          (to_string p) ^
+          ")"
+      )
+    | Composition(p, q) -> (
+          (to_string p) ^
+          "|" ^
+          (to_string q)
+      )
+    | Prefix(p, q) -> (
+          (to_string p) ^
+          "." ^
+          (to_string q)
+      )
