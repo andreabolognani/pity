0f3acbbb07ddea106fe2d992bc44840413d5e040
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-07-20 21:57:53 +0200

    Put the name tests into a module.

--- tests/name-tests.scm
+++ tests/name-tests.scm
@@ -1,28 +1,30 @@
-#lang scheme
+(module name-tests scheme
 
-(require rackunit
-         rackunit/text-ui
-         pity/name)
+ (require rackunit
+          rackunit/text-ui
+          pity/name)
 
-(define name-tests
- (test-suite
-  "Tests for the pity/name module"
+ (define name-tests
+  (test-suite
+   "Tests for the pity/name module"
 
-  (test-case
-   "name->string"
-   (check-equal? (name->string (name "x")) "x"))
+   (test-case
+    "name->string"
+    (check-equal? (name->string (name "x")) "x"))
   
-  (test-case
-   "name-list->string with empty name list"
-   (check-equal? (name-list->string '()) ""))
+   (test-case
+    "name-list->string with empty name list"
+    (check-equal? (name-list->string '()) ""))
   
-  (test-case
-   "name-list->string with a single name"
-   (check-equal? (name-list->string (list (name "x"))) "x"))
+   (test-case
+    "name-list->string with a single name"
+    (check-equal? (name-list->string (list (name "x"))) "x"))
 
-  (test-case
-   "name-list->string with three names"
-   (let ([name-list (list (name "x") (name "y") (name "z"))])
-    (check-equal? (name-list->string name-list) "x,y,z")))))
+   (test-case
+    "name-list->string with three names"
+    (let ([name-list (list (name "x") (name "y") (name "z"))])
+     (check-equal? (name-list->string name-list) "x,y,z")))))
 
-(exit (run-tests name-tests))
+ (exit (run-tests name-tests))
+
+ (provide name-tests))
