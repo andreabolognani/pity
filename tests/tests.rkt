1b9439594685866b78b18e46ede742a603d55c31
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-08-09 19:03:01 +0200

    Implement names, free names and bound names.

--- tests/tests.rkt
+++ tests/tests.rkt
@@ -3,13 +3,15 @@
 (require rackunit
          rackunit/text-ui
          "print-tests.rkt"
-         "parser-tests.rkt")
+         "parser-tests.rkt"
+         "names-tests.rkt")
 
 (define pity-tests
  (test-suite
   "Tests for pity"
 
   print-tests
-  parser-tests))
+  parser-tests
+  names-tests))
 
 (exit (run-tests pity-tests))
