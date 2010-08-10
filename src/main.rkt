986b0142905a42c287e86ce23b00d3588499e3e1
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-08-10 19:03:08 +0200

    Show free and bound names in the REPL.

--- src/main.rkt
+++ src/main.rkt
@@ -3,9 +3,20 @@
 (require pity
          "repl.rkt")
 
+;; Pretty-print a set of names to a string
+(define (pretty-set s)
+  (let ([names (set-map s name->string)])
+    (string-append "{" (string-join names ",") "}")))
+
+;; Try to parse the line into a term.
+;; If succesful, print the term followed by its free and bound names
 (define (action line)
   (with-handlers ([(lambda (e) (exn:fail:read? e))
                    (lambda (e) (printf "ERR: Exception caught~n"))])
-    (printf "~a~n" (term->string (string->term line)))))
+    (let ([term (string->term line)])
+      (printf "Term        : ~a~n" (term->string term))
+      (printf "Free names  : ~a~n" (pretty-set (free-names term)))
+      (printf "Bound names : ~a~n" (pretty-set (bound-names term))))))
 
-(repl action "> ")
+;; Start the evaluation loop
+(repl action "pity> ")
