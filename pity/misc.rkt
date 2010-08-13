2ebd1c0ca1f23a7cdf9ec6a84bcc3bb6dbd5d129
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-08-13 21:18:06 +0200

    Add some generic contracts.

--- pity/misc.rkt
+++ pity/misc.rkt
@@ -1,10 +1,51 @@
-#lang racket
+#lang at-exp racket
 
-(require "private/lexer.rkt"
+(require scribble/srcdoc
+         "private/lexer.rkt"
          "private/parser.rkt")
 
 (define (string->term str)
   (let ([ip (open-input-string str)])
     (private-parser (lambda () (private-lexer ip)))))
+
+
+;; Contracts
+;; ---------
+;;
+;;  Pretty generic contracts one would expect to find built-in.
+
+(provide/doc
+  (proc-doc setof
+            (([c contract?]) () . ->d . [_ contract?])
+            @{Returns a contract that recognizes a set whose every
+              element matches the contract @scheme[c].}))
+(define (setof c)
+  (let ([band (lambda (a b) (and a b))] ; Binary and wrapper for fold
+        [c-name (symbol->string (object-name c))])
+    (flat-named-contract
+      (string-append "(setof " c-name ")")
+      (lambda (x)
+        (and (set? x)
+             (foldl band #t (set-map x c)))))))
+
+(provide/doc
+  (proc-doc non-empty-setof
+            (([c contract?]) () . ->d . [_ contract?])
+            @{Returns a contract that recognizes non-empty sets whose
+              every element matches the contract @scheme[c].}))
+(define (non-empty-setof c)
+  (let ([c-name (symbol->string (object-name c))])
+    (flat-named-contract
+      (string-append "(non-empty-setof " c-name ")")
+      (and/c (setof c) (not/c set-empty?)))))
+
+(provide/doc
+  (proc-doc non-empty-string?
+            (([v any/c]) () . ->d . [_ boolean?])
+            @{Returns @scheme[#t] if @scheme[v] is a non-empty
+              string, @scheme[#f] otherwise.}))
+(define (non-empty-string? x)
+  (and (string? x)
+       (not (= (string-length x) 0))))
 
 (provide string->term)
