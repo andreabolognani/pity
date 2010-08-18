7e8cabf9be636ba9644d0709bf26066a108e8ee1
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-08-18 21:10:51 +0200

    Explain why string->process is in the private module.

--- pity/private.rkt
+++ pity/private.rkt
@@ -5,6 +5,12 @@
          "contracts.rkt"
          "process.rkt")
 
+;; This really belongs to the process module, and is documented as
+;; such, but putting it there causes a require cycle.
+;;
+;; Another way to work around the cycle would be to embed the parser
+;; into the process module. I might end up doing that.
+
 (define (string->process str)
   (let ([ip (open-input-string str)])
     (private-parser (lambda () (private-lexer ip)))))
