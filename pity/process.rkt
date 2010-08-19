045ab6413b21b87e32307f9c0a0f661b72cbfafd
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-08-19 19:41:07 +0200

    Cosmetic changes to the process module.
    Use better names for the procedures used by process->string;
    modify them so there’s no need for the extra match-let.

--- pity/process.rkt
+++ pity/process.rkt
@@ -14,6 +14,7 @@
 (define-struct prefix      (p q) #:transparent)
 
 
+;; Recognize any kind of process
 (define (process? v)
   (or
     (nil? v)
@@ -32,6 +33,7 @@
 ;;  and several smaller procedures which calculate the free and bound
 ;;  names for a single type of process.
 
+
 ;; Find the names that have free occurences in a process
 (define (free-names process)
   (match process
@@ -42,6 +44,7 @@
     [(restriction x p) (free-names/restriction x p)]
     [(composition p q) (free-names/composition p q)]
     [(prefix p q)      (free-names/prefix p q)]))
+
 
 ;; Find the names that have bound occurences in a process
 (define (bound-names process)
@@ -54,6 +57,7 @@
     [(composition p q) (bound-names/composition p q)]
     [(prefix p q)      (bound-names/prefix p q)]))
 
+
 ;; Find all the names occurring in a process
 (define (names process)
   (match process
@@ -64,6 +68,7 @@
     [(restriction x p) (names/restriction x p)]
     [(composition p q) (names/composition p q)]
     [(prefix p q)      (names/prefix p q)]))
+
 
 ;; Names, free names and bound names in the nil process
 
@@ -76,6 +81,7 @@
 (define (names/nil)
   (set))
 
+
 ;; Names, free names and bound names in a replication
 
 (define (free-names/replication p)
@@ -86,6 +92,7 @@
 
 (define (names/replication p)
   (names p))
+
 
 ;; Names, free names and bound names in an input action
 
@@ -99,6 +106,7 @@
   (set-union (set x)
              (list->set y)))
 
+
 ;; Names, free names and bound names in an output action
 
 (define (free-names/output x y)
@@ -111,6 +119,7 @@
 (define (names/output x y)
   (set-union (set x)
              (list->set y)))
+
 
 ;; Names, free names and bound names under a restriction
 
@@ -156,69 +165,82 @@
              (names q)))
 
 
+;; Process pretty-printing
+;; -----------------------
+;;
+;;  When a process is converted to a string, only strictly needed
+;;  parentheses should be present.
+;;
+;;  More importantly, it must be possible to parse the returned string
+;;  and obtain the same process once again.
+
+
+;; Convert a process to a string
 (define (process->string process)
   (match process
     [(nil)             "0"]
-    [(replication p)   (replication->string process)]
-    [(input x y)       (input->string process)]
-    [(output x y)      (output->string process)]
-    [(restriction x p) (restriction->string process)]
-    [(composition p q) (composition->string process)]
-    [(prefix p q)      (prefix->string process)]))
+    [(replication p)   (process->string/replication p)]
+    [(input x y)       (process->string/input x y)]
+    [(output x y)      (process->string/output x y)]
+    [(restriction x p) (process->string/restriction x p)]
+    [(composition p q) (process->string/composition p q)]
+    [(prefix p q)      (process->string/prefix p q)]))
 
-; replication->string : replication -> string
-(define (replication->string process)
-  (match-let ([(replication p) process])
-    (string-append "!"
-                   (if (contains-composition? p)
-                       (enclose (process->string p))
-                       (process->string p)))))
 
-; input->string : output -> string
-(define (input->string process)
-  (match-let ([(input x y) process])
-    (string-append (name->string x) "("
-                   (name-list->string y) ")")))
+;; Convert a replication to a string
+(define (process->string/replication p)
+  (string-append "!"
+                 (if (contains-composition? p)
+                     (enclose (process->string p))
+                     (process->string p))))
 
-; output->string : output -> string
-(define (output->string process)
-  (match-let ([(output x y) process])
-    (string-append (name->string x) "<"
-                   (name-list->string y) ">")))
 
-; restriction->string : restriction -> string
-(define (restriction->string process)
-  (match-let ([(restriction x p) process])
-    (string-append (enclose (name->string x))
-                   (if (contains-composition? p)
-                       (enclose (process->string p))
-                       (process->string p)))))
+;; Convert an input to a string
+(define (process->string/input x y)
+  (string-append (name->string x) "("
+                 (name-list->string y) ")"))
 
-; composition->string : composition -> string
-(define (composition->string process)
-  (match-let ([(composition p q) process])
-    (string-append (process->string p)
-                   "|"
-                   (process->string q))))
 
-; prefix->string : prefix -> string
-(define (prefix->string process)
-  (match-let ([(prefix p q) process])
-    (string-append (if (contains-composition? p)
-                       (enclose (process->string p))
-                       (process->string p))
-                   "."
-                   (if (contains-composition? q)
-                       (enclose (process->string q))
-                       (process->string q)))))
+;; Convert an output to a string
+(define (process->string/output x y)
+  (string-append (name->string x) "<"
+                 (name-list->string y) ">"))
+
+
+;; Convert a restriction to a string
+(define (process->string/restriction x p)
+  (string-append (enclose (name->string x))
+                 (if (contains-composition? p)
+                     (enclose (process->string p))
+                     (process->string p))))
+
+
+;; Convert a composition to a string
+(define (process->string/composition p q)
+  (string-append (process->string p)
+                 "|"
+                 (process->string q)))
+
+
+;; Convert a prefix to a string
+(define (process->string/prefix p q)
+  (string-append (if (contains-composition? p)
+                     (enclose (process->string p))
+                     (process->string p))
+                 "."
+                 (if (contains-composition? q)
+                     (enclose (process->string q))
+                     (process->string q))))
 
 
 ;; Utility functions
 ;; -----------------
 
+
 ;; Enclose a string between matching parentheses
 (define (enclose stuff)
   (string-append "(" stuff ")"))
+
 
 ;; Checks whether a process contains a composition.
 ;; Processes containing compositions will need to be enclosed
