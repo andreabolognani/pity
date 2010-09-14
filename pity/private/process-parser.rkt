29bd280ddc6ea42d65bd3c4f1440d9239d996198
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-09-14 12:37:52 +0200

    Re-indent the process parser.

--- pity/private/process-parser.rkt
+++ pity/private/process-parser.rkt
@@ -34,27 +34,34 @@
 
     (grammar
 
-      (process    [(subprocess)                         $1]
-                  [(subprocess PIPE process)            (composition $1 $3)])
+      (process
+        [(subprocess)                     $1]
+        [(subprocess PIPE process)        (composition $1 $3)])
 
-      (subprocess [(part)                               $1]
-                  [(BANG subprocess)                    (replication $2)])
+      (subprocess
+        [(part)                           $1]
+        [(BANG subprocess)                (replication $2)])
 
-      (part       [(term)                               $1]
-                  [(L_PAREN name R_PAREN part)          (restriction $2 $4)])
+      (part
+        [(term)                           $1]
+        [(L_PAREN name R_PAREN part)      (restriction $2 $4)])
 
-      (term       [(action)                             $1]
-                  [(action DOT term)                    (prefix $1 $3)])
+      (term
+        [(action)                         $1]
+        [(action DOT term)                (prefix $1 $3)])
 
-      (action     [(NIL)                                (nil)]
-                  [(name L_PAREN name_list R_PAREN)     (input $1 $3)]
-                  [(name L_BRACKET name_list R_BRACKET) (output $1 $3)]
-                  [(L_PAREN process R_PAREN)            $2])
+      (action
+        [(NIL)                            (nil)]
+        [(name L_PAREN names R_PAREN)     (input $1 $3)]
+        [(name L_BRACKET names R_BRACKET) (output $1 $3)]
+        [(L_PAREN process R_PAREN)        $2])
 
-      (name_list  [(name)                               (list $1)]
-                  [(name COMMA name_list)               (list* $1 $3)])
+      (names
+        [(name)                           (list $1)]
+        [(name COMMA names)               (list* $1 $3)])
 
-      (name       [(NAME)                               (name $1)]))))
+      (name
+        [(NAME)                           (name $1)]))))
 
 
 ; Export public symbols
