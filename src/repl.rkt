d5fd4dae03160ba44251e5ae3bd31af818bbc987
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-09-15 11:39:26 +0200

    Simplify REPL and make it properly tail recursive.

--- src/repl.rkt
+++ src/repl.rkt
@@ -20,10 +20,6 @@
 
 (define (repl action prompt)
   (printf "~a" prompt)
-  (repl-real action prompt))
-
-
-(define (repl-real action prompt)
   (let ([line (read-line)])
     (unless (eq? line eof)
       (begin (action line) (repl action prompt)))))
