baf9946783b2790d6896f069120a313de4e0e8bc
Andrea Bolognani <andrea.bolognani@roundhousecode.com>
2010-09-25 11:44:28 +0200

    Make the REPL useful.
    Instead of just reading stuff and parsing it, make it possible to
    assing values to names, show values attached to names, and check that
    a process respects a sorting.

--- src/main.rkt
+++ src/main.rkt
@@ -22,41 +22,84 @@
          "repl.rkt")
 
 
-; Pretty-print a set of names to a string
-(define (pretty-set s)
-  (let ([names (set-map s name->string)])
-    (string-append "{" (string-join names ",") "}")))
-
-
-; Try parsing the line as a process: if succesful, print the process
-; followed by its free and bound names, otherwise try parsing the line
-; as a sorting or as an environment
-(define (try-process-and-sorting-and-environment line)
+; Assign a value to a name, parsing it as a process.
+; If parsing as a process fails, try to parse it as a sorting.
+(define (op-set! vars n v)
   (with-handlers ([exn:fail:read?
-                   (lambda (e) (try-sorting-and-environment line))])
-    (let ([process (string->process line)])
-      (printf "Process     : ~a~n" (process->string process))
-      (printf "Free names  : ~a~n" (pretty-set (free-names process)))
-      (printf "Bound names : ~a~n" (pretty-set (bound-names process))))))
+                   (lambda (e) (op-set!/sorting vars n v))])
+    (hash-set vars n (string->process v))))
 
 
-; Try parsing the line as a sorting; if succesful, print the sorting,
-; otherwise try parsing the line as an environment
-(define (try-sorting-and-environment line)
+; Assign a value to a name, parsing it as a sorting.
+; If parsing fails, print an error message.
+(define (op-set!/sorting vars n v)
   (with-handlers ([exn:fail:read?
-                   (lambda (e) (try-environment line))])
-    (let ([sorting (string->sorting line)])
-      (printf "Sorting : ~a~n" (sorting->string sorting)))))
+                   (lambda (e) (printf "SET!: Invalid value~n") vars)])
+    (hash-set vars n (string->sorting v))))
 
 
-; Try parsing the line as an environment and print an error message if
-; the environment cannot be parsed
-(define (try-environment line)
-  (with-handlers ([exn:fail:read?
-                   (lambda (e) (printf "ERR: Exception caught~n"))])
-    (let ([environment (string->environment line)])
-      (printf "Environment : ~a~n" (environment->string environment)))))
+; Display a value according to its type.
+(define (op-display vars n)
+  (let ([v (hash-ref vars n #f)])
+    (cond
+      [(process? v) (printf "~a~n" (process->string v))]
+      [(sorting? v) (printf "~a~n" (sorting->string v))]
+      [else (printf "")])))
+
+
+; Check whether the process pointed to by n1 respects the sorting
+; pointed to by n2. If it does, print all the valid environments.
+(define (op-respects? vars n1 n2)
+  (let ([p (hash-ref vars n1 #f)]
+        [srt (hash-ref vars n2 #f)])
+    (if (and (process? p) (sorting? srt))
+        (let ([res (process-respects? p srt)])
+          (when res
+                (set-for-each
+                  res
+                  (lambda (env)
+                    (printf "~a~n" (environment->string env))))))
+        (printf "RESPECTS?: Need a process and a sorting~n"))))
+
+
+; Display an help message
+(define (op-help)
+  (printf "Operation   Arguments         Description ~n")
+  (printf "  SET!        NAME VALUE        Assign VALUE to NAME~n")
+  (printf "  DISPLAY     NAME              Show the value of NAME~n")
+  (printf "  RESPECTS?   PROCESS SORTING   Show environments in which ~n")
+  (printf "                                PROCESS respects SORTING~n")
+  (printf "  HELP                          Show this help message~n"))
+
+
+; Allow the procedure called by the REPL to maintain some state.
+;
+; The action-with-state procedure is called with two additional
+; arguments: the line number, and a hash table containing mappings
+; from variable names to values. The value returned by the procedure
+; is the updated hash table.
+(define action #f)
+(let ([lineno 0]
+      [vars (hash)])
+  (set! action (lambda (line)
+                 (set! vars (action-with-state line lineno vars))
+                 (set! lineno (+ lineno 1)))))
+
+
+; Parse the input and act accordingly
+(define (action-with-state line lineno vars)
+  (let* ([parts (regexp-split #rx" +" line)]
+         [op (car parts)]
+         [lop (if (< (length parts) 2) "" (cadr parts))]
+         [rop (if (< (length parts) 3) "" (apply string-append (cddr parts)))])
+    (cond
+      [(string-ci=? op "SET!") (set! vars (op-set! vars lop rop))]
+      [(string-ci=? op "DISPLAY") (op-display vars lop)]
+      [(string-ci=? op "RESPECTS?") (op-respects? vars lop rop)]
+      [(string-ci=? op "HELP") (op-help)]
+      [else (printf "Operation not supported ~a~n" op)])
+    vars))
 
 
 ; Start the evaluation loop
-(repl try-process-and-sorting-and-environment "pity> ")
+(repl action "pity> ")
